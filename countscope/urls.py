3ab8e2df9dece57bfb8c81d34f667ca7ba93f01b
aysee19 <aysee19>
2023-12-27 00:15:30 +0300

    html sayfaları eklendi

--- countscope/urls.py
+++ countscope/urls.py
@@ -5,8 +5,12 @@ from . import views
 #http://127.0.0.1:8000/home => anasayfa
 #http://127.0.0.1:8000/counter => tanımlama kısmı
 
-urlpatterns = [
-    path("",views.home),
-    path("anasayfa",views.home),
-    path("counter",views.counter),
+urlpatterns = [    
+    path('contact', views.contact),
+    path('about', views.about),
+    path("",views.home,name='courses_by_category'),
+    path("anasayfa",views.home , name='courses_by_category'),
+
+    path('<int:category_id>', views.getCoursesByCategoryId),
+    path('<str:category_name>', views.getCoursesByCategory, name='courses_by_category'),
 ]
--- countscope/views.py
+++ countscope/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    return HttpResponse("Anasayfa")
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-def counter(request):
-    return HttpResponse("Counter")
+# Create your views here.
+data = {
+    "photo":"photo kısmı",
+    "video":"video kısmı", 
+}
+def home(request):
+    # list_items=""
+    # category_list = list(data.keys())
+
+    # for category in category_list:
+    #     redirect_url= reverse('courses_by_category',args=[category])
+    #     list_items += f"<li><a href = '{redirect_url}'>{category}</a></li>"
+    
+    # html=f"<h1>seçenek listesi</h1><br><ul>{list_items}</ul>"
+    # return HttpResponse(html)    
+   return render(request, "index.html")
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def getCoursesByCategory(request, category_name):
+    try:
+        category_text = data[category_name];    
+        return HttpResponse(category_text)
+    except:
+        return HttpResponseNotFound("yanlış kategori seçimi")
+
+def getCoursesByCategoryId(request, category_id):
+    category_list = list(data.keys()) 
+    if(category_id > len(category_list)):
+        return HttpResponseNotFound("yanlış kategori seçimi")
+
+    category_name = category_list[category_id - 1]
+
+    redirect_url = reverse('courses_by_category', args=[category_name])
+
+    return redirect(redirect_url)
